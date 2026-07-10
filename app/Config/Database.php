a54a88142e13c5850347a9accdd9cf0a242f804b
prashanthbellam <prashanthbellam71@gmail.com>
2026-07-10 12:51:31 +0530

    database changes

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => '168.144.70.184',
-        'username'     => 'chandra',
-        'password'     => '8Wt8}dO09Ps(!',
-        'database'     => 'rmt_form_builder',
+        'hostname'     => 'localhost',
+        'username'     => 'root',
+        'password'     => '',
+        'database'     => 'formbuilder',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
